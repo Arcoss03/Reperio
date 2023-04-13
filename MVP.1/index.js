621f0c37cff99681cd810d85ee3d1b82b7cca3c3
Arthur-Frin <frin.arthur@gmail.com>
2023-04-13 20:17:02 +0200

    MVP.1 fini

--- MVP.1/index.js
+++ MVP.1/index.js
@@ -8,27 +8,56 @@ const db = mysql.createConnection({
     port: '8889',
     user: 'root',
     password: 'root',
-    database: 'MVP-DB',
+    database: 'db-mvp-projet',
   });
 
-// db.connect((err) => {
-//     if (err){
-//         throw err;
-//     }
-//     console.log('Mysql connected');
-// });
+db.connect((err) => {
+    if (err){
+        throw err;
+    }
+    console.log('Mysql connected');
+});
 
 app.listen('3000', () => {
     console.log("server started on port 3000");
 });
 
+//affichage fichier user-choice
 app.get("/", (req, res) => {
   res.sendFile('public/user-choice.html',{ root: __dirname });
 });
 
-app.get("/candidat", (req, res) => {
-    res.sendFile('public/candidat.html',{ root: __dirname });
+app.get('/liste/recruteur', (req, res) => {
+    db.query('SELECT name, description FROM recruteur', (err, results) => {
+        if (err) throw err;
+        let dataArray = JSON.parse(JSON.stringify(results));
+        let html = '<ul>';
+        for (let i = 0; i < dataArray.length; i++){
+            html += `<li>${dataArray[i].name}</li>`;
+            html += `<li>${dataArray[i].description}</li>`
+            html += '<li><input type="checkbox" id="profil6" name="profil6" value="1"></li><br><br>'
+        }
+        html += '</ul>';
+        html += '<a href="/"><button>Retour</button></a>'
+        res.send(html);
+    });
 });
-app.get("/recruteur", (req, res) => {
-    res.sendFile('public/recruteur.html',{ root: __dirname });
+
+app.get('/liste/candidats', (req, res) => {
+    db.query('SELECT name, lastname, tel, certification, description FROM `candidat`', (err, results) => {
+        if (err) throw err;
+        let dataArray = JSON.parse(JSON.stringify(results));
+        let html = '<ul>';
+        for (let i = 0; i < dataArray.length; i++){
+            html += `<li>${dataArray[i].name}</li>`;
+            html += `<li>${dataArray[i].lastname}</li>`;
+            html += `<li>${dataArray[i].tel}</li>`;
+            html += `<li>${dataArray[i].certification}</li>`
+            html += `<li>${dataArray[i].description}</li>`
+            html += '<li><input type="checkbox" id="profil6" name="profil6" value="1"></li><br><br>'
+        }
+        html += '</ul>';
+        html += '<a href="/"><button>Retour</button></a>'
+        res.send(html);
+    });
 });
