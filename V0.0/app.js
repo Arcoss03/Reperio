5dba86bcd842c5a5e32f41a3d6dcf393835ea651
Arthur-Frin <frin.arthur@gmail.com>
2023-04-18 11:51:59 +0200

    ajout liste des requetes dans back

--- V0.0/app.js
+++ V0.0/app.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const path = require('path');
-const indexRouter = require('./routes/index');
-const adminRouter = require('./routes/admin');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
+const indexRouter = require("./routes/index");
+const adminRouter = require("./routes/admin");
 
 const app = express();
-const PORT = '3002';
+const PORT = "3003";
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use('/', indexRouter);
-app.use('/admin', adminRouter);
+app.use("/", indexRouter);
+app.use("/admin", adminRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
