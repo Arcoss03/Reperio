6ac3d5b3243f2916fe3d39e90762e86784a3fe65
Clément Kerviche <kerviche.clement@gmail.com>
2023-04-19 19:03:46 +0200

    Ajout du swipe

--- Front/scripts/formulaire.js
+++ Front/scripts/formulaire.js
@@ -1,14 +1,17 @@
 const prenomInput = document.getElementById('prenom');
 const nomInput = document.getElementById('nom');
 const slideCheckbox = document.getElementById('slide');
+const selectuser = document.getElementById('selectuser');
 
 function updatePlaceholders() {
     if (slideCheckbox.checked) {
         prenomInput.placeholder = 'Nom de l\'entreprise';
         nomInput.placeholder = 'Liste des domaines';
+        selectuser.innerHTML = 'Entreprise';
     } else {
         prenomInput.placeholder = 'Prénom';
         nomInput.placeholder = 'Nom';
+        selectuser.innerHTML = 'Candidat';
     }
 }
 
