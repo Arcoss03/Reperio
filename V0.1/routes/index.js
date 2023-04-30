810409fd118eb525afe12cbe7f14a23a8ca96690
Arthur-Frin <frin.arthur@gmail.com>
2023-04-30 19:54:36 +0200

    ajout upload fichier

--- V0.1/routes/index.js
+++ V0.1/routes/index.js
@@ -4,6 +4,52 @@ const connection = require("../config/db");
 const path = require("path");
 const hashing = require("../config/hashing");
 const bcrypt = require("bcryptjs");
+const fs = require("fs");
+const multer = require("multer");
+const upload = multer({ dest: "public/uploads/" });
+
+//route de creation de compte
+router.post("/create", upload.single("photo"), (req, res) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const email = req.body.email;
+  const firstname = req.body.firstname;
+  const surname = req.body.surname;
+  const password = hashing(req.body.password);
+  const password2 = hashing(req.body.password2);
+
+  if (
+    password === password2 &&
+    emailRegex.test(email) &&
+    firstname &&
+    surname
+  ) {
+    const fileExtension = path.extname(req.file.originalname);
+    const fileName = `${firstname}_${surname}_${Date.now()}${fileExtension}`;
+    const filePath = path.join(__dirname, "../public/uploads", fileName);
+
+    // Renommer le fichier avant de le sauvegarder
+    fs.rename(req.file.path, filePath, (error) => {
+      if (error) throw error;
+
+      connection.query(
+        `INSERT INTO users (email, firstname, surname, password, photo) 
+          VALUES ('${email}', '${firstname}', '${surname}', '${password}', '${fileName}');`,
+        (error, results, fields) => {
+          if (error) throw error;
+          console.log("New user has been added");
+
+          res.send(
+            `le compte à bien été enregistré <a href="/"><button>retour à la page de connexion</button></a> ${req.file}`
+          );
+        }
+      );
+    });
+  } else {
+    res.send(
+      `email invalide ou mot de passe incorect <a href="/register"><button>retour</button></a>`
+    );
+  }
+});
 
 // Route de test
 router.get("/test", (req, res) => {
@@ -20,38 +66,6 @@ router.get("/register", (req, res) => {
   res.sendFile("register.html", { root: "public" });
 });
 
-//Route de création de compte
-router.post("/create", (req, res) => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const email = req.body.email;
-  const firstname = req.body.firstname;
-  const surname = req.body.surname;
-  const password = hashing(req.body.password);
-  const password2 = hashing(req.body.password2);
-  if (
-    password === password2 &&
-    emailRegex.test(email) &&
-    firstname &&
-    surname
-  ) {
-    connection.query(
-      `INSERT INTO users (email, firstname, surname, password) 
-          VALUES ('${email}', '${firstname}', '${surname}', '${password}');`,
-      (error, results, fields) => {
-        if (error) throw error;
-        console.log("New user has been added");
-        res.send(
-          `le compte à bien été enregistré <a href="/"><button>retour à la page de connexion</button></a>`
-        );
-      }
-    );
-  } else {
-    res.send(
-      `email invalide ou mot de passe incorect <a href="/register"><button>retour</button></a>`
-    );
-  }
-});
-
 // Route de connexion
 router.post("/auth", (req, res) => {
   const email = req.body.email;
@@ -66,6 +80,7 @@ router.post("/auth", (req, res) => {
           req.session.email = email;
           req.session.firstname = results[0].firstname;
           req.session.surname = results[0].surname;
+          req.session.photo = results[0].photo;
           res.redirect("/home");
         } else {
           res.send(
@@ -87,6 +102,7 @@ router.get("/home", (req, res) => {
     res.render("home", {
       firstname: req.session.firstname,
       surname: req.session.surname,
+      photo: req.session.photo,
     });
   } else {
     res.redirect("/");
